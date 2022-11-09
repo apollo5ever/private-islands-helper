e001fe1aa8608ae522f318e7fdc64695d3ee2f8e
apollo5ever <backend@ambrosia.money>
2022-11-09 16:52:51 -0600

    added support for top-ups

---
+++ contract calls/topUp.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch-commonjs');
+
+async function topUp(address,tier,amount,auth){
+
+    var addSupporterData = JSON.stringify(
+        {
+            "jsonrpc": "2.0",
+            "id": "1",
+            "method": "scinvoke",
+            "params": {
+                "scid": "ce99faba61d984bd4163b31dd4da02c5bff32445aaaa6fc70f14fe0d257a15c3",
+                "ringsize": 2,
+                "sc_dero_deposit":amount,
+                "sc_rpc": [{
+                    "name": "entrypoint",
+                    "datatype": "S",
+                    "value": "TU"
+                },
+                {
+                    "name": "T",
+                    "datatype": "S",
+                    "value": tier
+                },
+                {
+                    "name": "S",
+                    "datatype": "S",
+                    "value": address
+                }]
+            }
+        }
+    )
+    fetch('http://127.0.0.1:10103/json_rpc',{
+        method:'POST',
+        body:addSupporterData,
+        headers:{'Authorization':auth,'Content-Type':'application/json'}
+    })
+}
+
+module.exports = topUp
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const fetch = require('node-fetch-commonjs');
 const fs = require('fs');
+const addSupporter = require('./contract calls/addSupporter')
+const topUp = require('./contract calls/topUp')
 
 
 var last_height = fs.readFileSync('./last_height.json');
@@ -57,8 +59,27 @@ async function getTransfers(){
                         tier = json.result.entries[i].payload_rpc[j].value
                     }
                 }
-                addSupporter(address,tier,amount)
+                addSupporter(address,tier,amount,auth)
                 
+            }else if(json.result.entries[i].dstport==2){
+                //topup
+                var address=""
+                var tier=""
+                var amount = json.result.entries[i].amount
+                for(var j=0;j<json.result.entries[i].payload_rpc.length;j++){
+                    
+                    if(json.result.entries[i].payload_rpc[j].name=="address"){
+                       address=json.result.entries[i].payload_rpc[j].value
+                    }
+                    if(json.result.entries[i].payload_rpc[j].name=="tier"){
+                        tier = json.result.entries[i].payload_rpc[j].value
+                    }
+                }
+                topUp(address,tier,amount,auth)
+            }else if(json.result.entries[i].dstport==3){
+                //fundraiser
+            }else if(json.result.entries[i].dstport==4){
+                //bounty
             }
         }
     }
@@ -99,41 +120,7 @@ async function getHeight(){
     })
 }
 
-async function addSupporter(address,tier,amount){
 
-    var addSupporterData = JSON.stringify(
-        {
-            "jsonrpc": "2.0",
-            "id": "1",
-            "method": "scinvoke",
-            "params": {
-                "scid": "ce99faba61d984bd4163b31dd4da02c5bff32445aaaa6fc70f14fe0d257a15c3",
-                "ringsize": 2,
-                "sc_dero_deposit":amount,
-                "sc_rpc": [{
-                    "name": "entrypoint",
-                    "datatype": "S",
-                    "value": "AS"
-                },
-                {
-                    "name": "T",
-                    "datatype": "S",
-                    "value": tier
-                },
-                {
-                    "name": "S",
-                    "datatype": "S",
-                    "value": address
-                }]
-            }
-        }
-    )
-    fetch('http://127.0.0.1:10103/json_rpc',{
-        method:'POST',
-        body:addSupporterData,
-        headers:{'Authorization':auth,'Content-Type':'application/json'}
-    })
-}
 
 setInterval(
     getTransfers,600000
